99684da0e0ab7c6f87ab53cab0ae9351251d85b2
Outrun Colors <itsoutruncolors@gmail.com>
2022-08-25 14:36:02 -0500

    Change /lottery route to /casino and replace icon with usd symbol and change sitewide const to reflect change

--- files/helpers/wrappers.py
+++ files/helpers/wrappers.py
@@ -170,7 +170,7 @@ def lottery_required(f):
 	def wrapper(*args, **kwargs):
 		v = get_logged_in_user()
 
-		if not LOTTERY_ENABLED: abort(404)
+		if not CASINO_ENABLED: abort(404)
 		if v and not v.can_gamble: abort(403)
 
 		return make_response(f(v=v))
--- files/routes/casino.py
+++ files/routes/casino.py
@@ -4,9 +4,11 @@ from files.helpers.alerts import *
 from files.helpers.get import *
 from files.helpers.const import *
 from files.helpers.wrappers import *
+from files.helpers.lottery import *
 
 
 @app.get("/casino")
 @auth_required
 def casino(v):
-    return render_template("casino.html", v=v)
+    participants = get_users_participating_in_lottery()
+    return render_template("casino.html", v=v, participants=participants)
