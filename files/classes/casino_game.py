0c640ef7cad7f5a6571632cb750af8b0deecde32
Outrun Colors <itsoutruncolors@gmail.com>
2022-08-31 20:01:27 -0500

    Add sql migration file

--- files/classes/casino_game.py
+++ files/classes/casino_game.py
@@ -4,33 +4,6 @@ from files.helpers.lazy import lazy
 from files.helpers.const import *
 import time
 
-# --
-# -- Name: casino_game_kind; Type: ENUM; Schema: public; Owner: -
-# --
-
-# CREATE TYPE public.casino_game_kind AS ENUM ('blackjack', 'slots');
-
-# --
-# -- Name: casino_game_currency; Type: ENUM; Schema: public; Owner: -
-# --
-
-# CREATE TYPE public.casino_game_currency AS ENUM ('coins', 'procoins');
-
-# --
-# -- Name: casino_games; Type: TABLE; Schema: public; Owner: -
-# --
-
-# CREATE TABLE public.casino_games (
-#     id SERIAL PRIMARY KEY,
-#     user_id integer NOT NULL REFERENCES public.users(id),
-#     created_utc integer NOT NULL,
-#     active boolean NOT NULL DEFAULT true,
-#     currency public.casino_game_currency NOT NULL,
-#     wager integer NOT NULL,
-#     winnings integer NOT NULL, -- doubles as result, Δwinnings
-#     kind public.casino_game_kind NOT NULL,
-#     game_state jsonb NOT NULL
-# );
 
 class Casino_Game(Base):
     __tablename__ = "casino_games"
---
+++ sql/20220831-casino-feature.sql
@@ -0,0 +1,19 @@
+CREATE TYPE casino_game_kind AS ENUM ('blackjack', 'slots');
+
+CREATE TYPE casino_game_currency AS ENUM ('coins', 'procoins');
+
+CREATE TABLE casino_games (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id),
+    created_utc integer NOT NULL,
+    active boolean NOT NULL DEFAULT true,
+    currency casino_game_currency NOT NULL,
+    wager integer NOT NULL,
+    winnings integer NOT NULL, -- doubles as result, Δwinnings
+    kind casino_game_kind NOT NULL,
+    game_state jsonb NOT NULL
+);
+
+ALTER TABLE comments DROP COLUMN slots_result;
+
+ALTER TABLE comments DROP COLUMN blackjack_result;
